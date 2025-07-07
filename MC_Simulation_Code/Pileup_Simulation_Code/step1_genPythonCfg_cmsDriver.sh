e043ad2071f0c5cd5ec07a8f35373c850ffba7fb
Haozhen Xu <hx28@rice.edu>
2025-07-07 11:55:56 +0800

    Update step1_genPythonCfg_cmsDriver.sh

--- MC_Simulation_Code/Pileup_Simulation_Code/step1_genPythonCfg_cmsDriver.sh
+++ MC_Simulation_Code/Pileup_Simulation_Code/step1_genPythonCfg_cmsDriver.sh
@@ -1,53 +1,53 @@
 cmsDriver.py Configuration/GenProduction/python/HINPbPbAutumn18GS_STARlight_fragment.py \
---filein /store/group/phys_heavyions/xuha/0619_PU_Simulation/CohJpsi_lheFiles/slight_CohJpsi_0006.lhe \
+--filein /store/group/phys_heavyions/xuha/0619_PU_Simulation/CohJpsi_lheFiles/slight_CohJpsi_0010.lhe \
 --filetype LHE \
---fileout file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/step1_PU_output_files/step1_0006_OO.root \
---pileup_input filelist:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/pu_filelist.txt \
+--fileout file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_Output_Files/Step1/step1_PU_0010_OO.root \
+--pileup HiMixGEN \
+--pileup_input /store/group/phys_heavyions/xuha/Pileup_Events/MinBias_Hijing_OO_5362GeV_10.root \
 --mc \
 --eventcontent RAWSIM \
 --datatier GEN-SIM \
---conditions 141X_mcRun3_2024_realistic_HI_v14 \
+--conditions 150X_mcRun3_2025_forOO_realistic_v5 \
 --beamspot DBrealistic \
 --step GEN,SIM \
 --nThreads 1 \
 --scenario HeavyIons \
 --geometry DB:Extended \
 --era Run3_2025_OXY \
---python_filename /afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/RunFiles/step1_STARlight_LHE_GenSim_cfg.py \
+--python_filename /afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_RunFiles/step1_STARlight_LHE_GenSim_cfg.py \
 --no_exec \
 --customise Configuration/DataProcessing/Utils.addMonitoring \
--n 1000
+-n 500
 
-cmsDriver.py Configuration/GenProduction/python/MinBias_Hijing_OO_5362GeV.py \
-  --filein file:step1_PU_output_files/step1_0006_OO.root \
-  --fileout file:step2_output_files/step2_0006_OO.root \
-  --mc --eventcontent RAWSIM \
-  --datatier GEN-SIM \
-  --conditions 150X_mcRun3_2025_forOO_realistic_v5 \
-  --beamspot MatchHI \
-  --step GEN,SIM \
-  --nThreads 4 \
-  --scenario HeavyIons \
-  --geometry DB:Extended \
-  --era Run3_2025_OXY \
-  --pileup_input filelist:pu_filelist.txt
-  --pileup HiMixGEN \
-  --python_filename step2_STARlight_GENSIM_PU_cfg.py \
-  --customise Configuration/DataProcessing/Utils.addMonitoring \
-  --no_exec \
-  -n 1000
+cmsDriver.py \
+--filein file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_Output_Files/Step1/step1_PU_0010_OO.root \
+--fileout file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_Output_Files/Step2/step2_PU_0010_OO.root \
+--mc \
+--eventcontent RAWSIM \
+--pileup HiMix \
+--pileup_input /store/group/phys_heavyions/xuha/Pileup_Events/MinBias_Hijing_OO_5362GeV_10.root \
+--datatier GEN-SIM-DIGI-RAW \
+--conditions 150X_mcRun3_2025_forOO_realistic_v5 \
+--step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:@fake2 \
+--geometry DB:Extended \
+--era Run3_2025_OXY \
+--customise_commands "process.RAWSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*'])" \
+--python_filename /afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_RunFiles/step2_STARlight_Digi_cfg.py \
+--no_exec \
+-n 500 \
+--nThreads 8
 
-  cmsDriver.py step3 \
-  --mc \
-  --eventcontent RAWSIM \
-  --pileup HiMix \
-  --datatier GEN-SIM-DIGI-RAW \
-  --conditions 150X_mcRun3_2025_forOO_realistic_v5 \
-  --step DIGI:pdigi_hi_nogen,L1,DIGI2RAW,HLT:@fake2 \
-  --geometry DB:Extended \
-  --era Run3_2025_OXY \
-  --pileup_input "file:step2_PU_output_files/step2_0006_OO.root"  \
-  --customise_commands "process.RAWSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*'])" \
-  --filein "file:step1_output_files/step1_0006_OO.root" \
-  -n 1000 \
-  --nThreads 8
+cmsDriver.py \
+--filein file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_Output_Files/Step2/step2_PU_0010_OO.root \
+--fileout file:/afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_Output_Files/Step3/step3_PU_0010_OO.root \
+--mc \
+--eventcontent AODSIM \
+--datatier AODSIM \
+--conditions 150X_mcRun3_2025_forOO_realistic_v5 \
+--step RAW2DIGI,L1Reco,RECO \
+--era Run3_2025_OXY \
+--customise_commands "process.AODSIMoutput.outputCommands.extend(['keep *_mix_MergedTrackTruth_*', 'keep *Link*_simSiPixelDigis__*', 'keep *Link*_simSiStripDigis__*', 'keep *_generalTracks__*', 'keep *_hiConformalPixelTracks__*', 'keep *_siPixelClusters__*', 'keep *_siStripClusters__*'])" \
+--python_filename /afs/cern.ch/user/x/xuha/CMSSW_15_0_8/src/PU_RunFiles/step3_STARlight_Reco_cfg.py \
+--no_exec \
+-n 500 \
+--nThreads 8
